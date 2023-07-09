e5c35dd2c771ff01ec7ccce5cd6f734047b1b19b
“[LashaDemurashvili]” <lasha.demurashvili.1@iliauni.edu.ge>
2023-07-09 16:31:32 +0400

    save 04

--- script.js
+++ script.js
@@ -1,33 +1,17 @@
-// document.querySelector(".test-btn").addEventListener('click', function (){
-//     const box = document.createElement("div");
-//     box.className = "box";
-//     document.body.appendChild(box);
-//     box.style.backgroundColor = "#e30404";
-//
-// })
-//
-
-
-$(document).ready(function () {
+$(document).ready(function (){
     let id_counter = 0;
-    const text_header = $("#text-label-header").val()
-    const text_desc = $("#text-input").val()
+    $(".submit-btn").click(function() {
+        id_counter ++;
 
-    let div_clone = $(".box").clone()
-
-    $(".submit-btn").click(function () {
-        $(".textContainer").css({"visibility": "visible"})
-
-        $(".box").attr("id", `box-id-${id_counter}`)
-        $(".text-title").attr("id", `text-title-id-${id_counter}`).text(text_header)
-        $(".text-description").attr("id", `text-description-id-${id_counter}`).text(text_desc)
+        let text_header = $("#text-label-header").val()
+        let text_desc = $("#text-input").val()
 
 
-        // $(".text-title").text(text_header)
-        // $(".text-description").text(text_desc)
-        alert("Submitted")
-
+        alert(`${text_header} ${text_desc}`)
     })
+
+
+
 
     $(".edit-btn").click(function (){
         alert("Edited")
@@ -35,14 +19,11 @@ $(document).ready(function () {
 
 
     $(".del-btn").click(function (){
-        let get_box_id = $(".box").attr("id")
-        let box_id = document.getElementById(get_box_id)
-        $(box_id).remove()
-
+        alert("Deleted")
     })
+
+
 })
 
 
-// $(".box").addClass(`id_${id_counter++}`)
 
-// $(".element").attr("id","SomeID");
